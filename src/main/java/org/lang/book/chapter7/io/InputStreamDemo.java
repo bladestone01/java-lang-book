f8c03e52155a21d4eb520a45b6a314287f1d5cf5
bladestone <chengjunf2313_cn@126.com>
2018-04-10 09:03:55 +0800

    Java chapter 7 code.

---
+++ src/main/java/org/lang/book/chapter7/io/InputStreamDemo.java
@@ -0,0 +1,18 @@
+package org.lang.book.chapter7.io;
+
+/**
+ * https://www.cnblogs.com/ylspace/p/8128112.html
+ * https://blog.csdn.net/jianghuxiaojin/article/details/52073031
+ * https://www.ibm.com/developerworks/cn/java/j-lo-javaio/
+ * 
+ * @author tonychen
+ *
+ */
+public class InputStreamDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
